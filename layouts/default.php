62a5db482bced4b8dd24b32269a5458387d356e3
Matt Thomas <matt@betweenbrain.com>
2013-01-17 12:03:40 -0500

    Moving includes to common header and footer to layout file

--- layouts/default.php
+++ layouts/default.php
@@ -10,8 +10,13 @@
  * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v3 or later
  */
-?>
+// Load common header
+if (JFile::exists($header)) {
+	include $header;
+}
 
+?>
+<h1>Default Layout</h1>
 <?php if ($this->countModules('nav')) : ?>
 <nav id="nav" class="clear clearfix">
 	<jdoc:include type="modules" name="nav" style="raw" />
@@ -23,3 +28,14 @@
 <?php endif ?>
 
 <jdoc:include type="component" />
+
+<?php
+// Load common footer
+if (JFile::exists($footer)) {
+	include $footer;
+}
+
+
+
+
+
