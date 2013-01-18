adf80e173f7b744d8917fb4274ed5e0265fc9b69
Matt Thomas <matt@betweenbrain.com>
2013-01-18 12:23:21 -0500

    Template override to alter voting to act as mechanism for marking as popular (5 out of five stars)

--- html/com_k2/templates/video/item.php
+++ html/com_k2/templates/video/item.php
@@ -179,17 +179,12 @@ defined('_JEXEC') or die;
 	<?php if($this->item->params->get('itemRating')): ?>
 	<!-- Item Rating -->
 	<div class="itemRatingBlock">
-		<span><?php echo JText::_('K2_RATE_THIS_ITEM'); ?></span>
 		<div class="itemRatingForm">
 			<ul class="itemRatingList">
 				<li class="itemCurrentRating" id="itemCurrentRating<?php echo $this->item->id; ?>" style="width:<?php echo $this->item->votingPercentage; ?>%;"></li>
-				<li><a href="#" rel="<?php echo $this->item->id; ?>" title="<?php echo JText::_('K2_1_STAR_OUT_OF_5'); ?>" class="one-star">1</a></li>
-				<li><a href="#" rel="<?php echo $this->item->id; ?>" title="<?php echo JText::_('K2_2_STARS_OUT_OF_5'); ?>" class="two-stars">2</a></li>
-				<li><a href="#" rel="<?php echo $this->item->id; ?>" title="<?php echo JText::_('K2_3_STARS_OUT_OF_5'); ?>" class="three-stars">3</a></li>
-				<li><a href="#" rel="<?php echo $this->item->id; ?>" title="<?php echo JText::_('K2_4_STARS_OUT_OF_5'); ?>" class="four-stars">4</a></li>
 				<li><a href="#" rel="<?php echo $this->item->id; ?>" title="<?php echo JText::_('K2_5_STARS_OUT_OF_5'); ?>" class="five-stars">5</a></li>
 			</ul>
-			<div id="itemRatingLog<?php echo $this->item->id; ?>" class="itemRatingLog"><?php echo $this->item->numOfvotes; ?></div>
+			<span><?php echo JText::_('Mark as Popular'); ?></span>
 			<div class="clr"></div>
 		</div>
 		<div class="clr"></div>
