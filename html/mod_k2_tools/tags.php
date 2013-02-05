8f54d2a1ef5cdf7632bfec83cff96208efd43abf
Matt Thomas <matt@betweenbrain.com>
2013-02-05 14:30:16 -0500

    Code formating to fix extra space

--- html/mod_k2_tools/tags.php
+++ html/mod_k2_tools/tags.php
@@ -19,9 +19,7 @@ defined('_JEXEC') or die;
 		<?php foreach ($tags as $tag): ?>
 		<?php if (!empty($tag->tag)): ?>
 			<li>
-				<a href="<?php echo $tag->link; ?>" title="<?php echo $tag->count . ' ' . JText::_('K2_ITEMS_TAGGED_WITH') . ' ' . K2HelperUtilities::cleanHtml($tag->tag); ?>">
-					<?php echo $tag->tag; ?>
-				</a>
+				<a href="<?php echo $tag->link; ?>" title="<?php echo $tag->count . ' ' . JText::_('K2_ITEMS_TAGGED_WITH') . ' ' . K2HelperUtilities::cleanHtml($tag->tag); ?>"><?php echo $tag->tag; ?></a>
 				<?php echo $tag->count ?>
 			</li>
 			<?php endif; ?>
