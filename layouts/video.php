ea7137da1e5a8e6872ec0391b9f1d0d8c760b76f
Matt Thomas <matt@betweenbrain.com>
2013-03-19 15:24:39 -0400

    Add top-edge elements Add container divs

--- layouts/video.php
+++ layouts/video.php
@@ -19,61 +19,71 @@ if (JFile::exists($header)) {
 }
 
 ?>
-<?php if ($this->countModules('nav')) : ?>
-<nav id="nav" class="clear clearfix">
-	<jdoc:include type="modules" name="nav" style="raw" />
-</nav>
-<?php endif ?>
-
-<?php if ($this->getBuffer('message')) : ?>
-<jdoc:include type="message" />
-<?php endif ?>
-
-<div class="tags clearfix">
-	<jdoc:include type="modules" name="tags" style="xhtml"/>
-</div>
-
-<jdoc:include type="component" />
-<?php // Load modules onlly if K2 view is an itemlist (category)
-if (JRequest::getCmd('view', 0) == "itemlist") : ?>
-<div id="content-below" class="clearfix <?php echo $contentBelowClass ?>">
-	<?php if ($this->countModules('content-below-1')) : ?>
-	<div class="content-below-1 clearfix">
-		<jdoc:include type="modules" name="content-below-1" style="xhtml"/>
+<header>
+	<div class="top-edge">
+		<span class="red"></span><span class="gold"></span><span class="teal"></span><span class="blue"></span><span class="gold"></span><span class="green"></span>
 	</div>
-	<?php endif ?>
-
-	<?php if ($this->countModules('content-below-2')) : ?>
-	<div class="content-below-2 clearfix">
-		<jdoc:include type="modules" name="content-below-2" style="xhtml"/>
+	<div class="container">
+		<?php if ($this->countModules('nav')) : ?>
+			<nav class="clear clearfix">
+				<jdoc:include type="modules" name="nav" style="raw" />
+			</nav>
+		<?php endif ?>
 	</div>
-	<?php endif ?>
+</header>
 
-	<?php if ($this->countModules('content-below-3')) : ?>
-	<div class="content-below-3 clearfix">
-		<jdoc:include type="modules" name="content-below-3" style="xhtml"/>
-	</div>
-	<?php endif ?>
+	<div class="container">
 
-	<?php if ($this->countModules('content-below-4')) : ?>
-	<div class="content-below-4 clearfix">
-		<jdoc:include type="modules" name="content-below-4" style="xhtml"/>
-	</div>
-	<?php endif ?>
+		<?php if ($this->getBuffer('message')) : ?>
+			<jdoc:include type="message" />
+		<?php endif ?>
 
-	<?php if ($this->countModules('content-below-5')) : ?>
-	<div class="content-below-5 clearfix">
-		<jdoc:include type="modules" name="content-below-5" style="xhtml"/>
-	</div>
-	<?php endif ?>
+		<div class="tags clearfix">
+			<jdoc:include type="modules" name="tags" style="xhtml" />
+		</div>
 
-	<?php if ($this->countModules('content-below-6')) : ?>
-	<div class="content-below-6 clearfix">
-		<jdoc:include type="modules" name="content-below-6" style="xhtml"/>
+		<jdoc:include type="component" />
+		<?php // Load modules onlly if K2 view is an itemlist (category)
+		if (JRequest::getCmd('view', 0) == "itemlist" && JRequest::getCmd('option') == "com_k2") : ?>
+			<div id="content-below" class="clearfix <?php echo $contentBelowClass ?>">
+				<?php if ($this->countModules('content-below-1')) : ?>
+					<div class="content-below-1 clearfix">
+						<jdoc:include type="modules" name="content-below-1" style="xhtml" />
+					</div>
+				<?php endif ?>
+
+				<?php if ($this->countModules('content-below-2')) : ?>
+					<div class="content-below-2 clearfix">
+						<jdoc:include type="modules" name="content-below-2" style="xhtml" />
+					</div>
+				<?php endif ?>
+
+				<?php if ($this->countModules('content-below-3')) : ?>
+					<div class="content-below-3 clearfix">
+						<jdoc:include type="modules" name="content-below-3" style="xhtml" />
+					</div>
+				<?php endif ?>
+
+				<?php if ($this->countModules('content-below-4')) : ?>
+					<div class="content-below-4 clearfix">
+						<jdoc:include type="modules" name="content-below-4" style="xhtml" />
+					</div>
+				<?php endif ?>
+
+				<?php if ($this->countModules('content-below-5')) : ?>
+					<div class="content-below-5 clearfix">
+						<jdoc:include type="modules" name="content-below-5" style="xhtml" />
+					</div>
+				<?php endif ?>
+
+				<?php if ($this->countModules('content-below-6')) : ?>
+					<div class="content-below-6 clearfix">
+						<jdoc:include type="modules" name="content-below-6" style="xhtml" />
+					</div>
+				<?php endif ?>
+			</div>
+		<?php endif ?>
 	</div>
-	<?php endif ?>
-</div>
-<?php endif ?>
 <?php
 // Load common footer
 if (JFile::exists($footer)) {
